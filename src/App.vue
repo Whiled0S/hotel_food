42458d52240ddeb15a7af8b451656e34c6556e41
nikitabesx <nik.besx@gmail.com>
2020-01-09 19:31:32 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,14 @@
     methods: {
       ...mapActions('cart', ['getCart'])
     },
+    watch: {
+      '$route':{
+        handler: (to,) => {
+          document.title = to.meta.title || 'QUAR';
+        },
+        immediate: true
+      }
+    },
     created() {
       if (!this.locationHash) return;
 
@@ -30,46 +38,5 @@
 </script>
 
 <style lang="less">
-  @import "helpers/transitions";
-  @import "helpers/fonts";
-  @import "helpers/carousel";
-  @import "assets/fonts/Themify/themify-icons.css";
-
-  html {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-
-  *,
-  *::before,
-  *::after {
-    -webkit-box-sizing: inherit;
-    -moz-box-sizing: inherit;
-    box-sizing: inherit;
-  }
-
-  html, body {
-    width: 100%;
-    height: 100%;
-    font-family: Tahoma, sans-serif;
-  }
-
-  body {
-    font-family: "Roboto", sans-serif;
-    font-size: 16px;
-    font-weight: normal;
-    font-style: normal;
-    color: #2b2423;
-  }
-
-  #app {
-    display: flex;
-    min-height: 100%;
-  }
-
-  .container {
-    width: 100%;
-    padding: 0 16px;
-  }
+  @import "style/main";
 </style>
--- src/router.js
+++ src/router.js
@@ -15,72 +15,80 @@ import store from './store';
 
 Vue.use(Router);
 
+const routes = [
+  {
+    path: '/',
+    name: 'default',
+    component: Default
+  },
+  {
+    path: '/thank-you/:id',
+    name: 'thank-you',
+    component: Receipt,
+    beforeEnter(to, from, next) {
+      store.dispatch('receipt/getOrder', {orderId: to.params.id});
+      next();
+    }
+  },
+  {
+    path: '/:location',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/:location/restaurant/:id',
+    name: 'restaurant',
+    component: Restaurant
+  },
+  {
+    path: '/:location/dish/:id',
+    name: 'dish',
+    component: Dish
+  },
+  {
+    path: '/:location/history',
+    name: 'history',
+    component: History,
+    beforeEnter(to, from, next) {
+      store.dispatch('history/getOrders');
+      next();
+    }
+  },
+  {
+    path: '/:location/terms',
+    name: 'terms',
+    component: Terms
+  },
+  {
+    path: '/:location/translate',
+    name: 'translate',
+    component: Translate
+  },
+  {
+    path: '/:location/cart',
+    name: 'cart',
+    component: Cart,
+    beforeEnter(to, from, next) {
+      if (from.name) {
+        store.dispatch('cart/resetCart').then(() => {
+          store.dispatch('cart/getCart');
+        });
+      }
+      next();
+    }
+  }
+];
+
+routes.forEach((route) => {
+  route.metha = {
+    title: 'QUAR'
+  };
+});
+
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'default',
-      component: Default
-    },
-    {
-      path: '/thank-you/:id',
-      name: 'thank-you',
-      component: Receipt,
-      beforeEnter(to, from, next) {
-        store.dispatch('receipt/getOrder', {orderId: to.params.id});
-        next();
-      }
-    },
-    {
-      path: '/:location',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/:location/restaurant/:id',
-      name: 'restaurant',
-      component: Restaurant
-    },
-    {
-      path: '/:location/dish/:id',
-      name: 'dish',
-      component: Dish
-    },
-    {
-      path: '/:location/history',
-      name: 'history',
-      component: History,
-      beforeEnter(to, from, next) {
-        store.dispatch('history/getOrders');
-        next();
-      }
-    },
-    {
-      path: '/:location/terms',
-      name: 'terms',
-      component: Terms
-    },
-    {
-      path: '/:location/translate',
-      name: 'translate',
-      component: Translate
-    },
-    {
-      path: '/:location/cart',
-      name: 'cart',
-      component: Cart,
-      beforeEnter(to, from, next) {
-        if (from.name) {
-          store.dispatch('cart/resetCart').then(() => {
-            store.dispatch('cart/getCart');
-          });
-        }
-        next();
-      }
-    }
-  ]
+  routes: routes
 });
 
 router.beforeEach((to, from, next) => {
